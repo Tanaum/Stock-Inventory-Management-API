71fbb6370b58f7b453a118c5db76aace197e7df6
Tanaum Masooma <135850652+Tanaum@users.noreply.github.com>
2025-07-07 16:02:05 +0500

    API psuedocode

--- main.py
+++ main.py
@@ -2,3 +2,30 @@
 
 from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+# retrieving info abt an item that has been scanned
+@app.route("/scanned/<ItemID>")
+def ScannedItemInfo(ItemID):
+    # send the itemid over to a function
+    # function returns data about the item
+    # use a function to decrement the item by 1 (for now)
+    # return only: item id, item price, item name
+    ...
+
+# for lets say an employee to look up the price of an item
+@app.route("/admin/<ItemID>")
+def GetInfo(ItemID):
+    # send itemid over to a function
+    # return the info
+    ...
+
+# to update the amount of items restocked
+@app.route("/admin/restock/<ItemID>-<RestockedAmount>")
+def Restock(ItemID, RestockedAmount):
+    # send this info to a function
+    # function will update each row accordingly
+    ...
+
+if __name__ == "__main__":
+    app.run(debug=True)
